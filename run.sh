526eb6b24e2847cd0de755c52943c314bd15620a
Zoltán Adamek <djscorchio@gmail.com>
2018-09-18 22:05:13 +0200

    Fixes script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 . virtualenv/bin/activate
 
-cat source.csv | ./tsv2csv.py >> source.real.csv # dammit KH, TSV != CSV
+cat source.csv | ./tsv2csv.py > source.real.csv # dammit KH, TSV != CSV
 rm output.ofx && ./virtualenv/bin/csv2ofx --verbose -m kh source.real.csv output.ofx
