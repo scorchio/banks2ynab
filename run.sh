fb8e9f150c67e4c4f458bf262bf3ddedd8841573
Zoltán Adamek <djscorchio@gmail.com>
2023-12-21 21:55:37 +0100

    Run script with tomorrow as an end date, otherwise csv2ofx won't extract everything

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 
 cat source.csv | python3 ./tsv2csv.py > source.real.csv # dammit KH, TSV != CSV
 rm output.ofx 2> /dev/null || echo > /dev/null
-./virtualenv/bin/csv2ofx -m kh source.real.csv output.ofx
+./virtualenv/bin/csv2ofx -m kh source.real.csv output.ofx --end $(date -v+1d +%Y-%m-%d)
